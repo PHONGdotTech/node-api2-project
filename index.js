b761e848eae2bf2face8082ab152521aee1d5179
Phong Yang <phongyang88@hotmail.com>
2020-02-11 15:55:39 -0600

    all end points work. no checks or tests in place

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require("express");
+const postsRouter = require("./posts/posts-router.js");
 
 const server = express();
 
 server.use(express.json());
+server.use("/api/posts", postsRouter)
 
 server.listen(5000, ()=>{
     console.log("Server is running on port 5000")
---
+++ posts/posts-router.js
@@ -0,0 +1,89 @@
+const express = require("express");
+const Posts = require("../data/db.js");
+const router = express.Router();
+
+//post a post
+router.post(`/`, (req, res)=>{
+    Posts.insert(req.body)
+    .then(insertedID => {
+        res.status(201).json(insertedID)
+    })
+    .catch(err =>{
+        console.log(err)
+        res.status(500).json({errorMessage: "error message"})
+    })
+})
+
+//post a comment
+router.post(`/:id/comments`, (req, res)=>{
+    Posts.insertComment(req.body)
+    .then(insertedID => {
+        res.status(201).json(insertedID)
+    })
+    .catch(err =>{
+        console.log(err)
+        res.status(500).json({errorMessage: "error message"})
+    })
+})
+
+//get all posts
+router.get(`/`, (req, res)=>{
+    Posts.find()
+    .then(posts => {
+        res.status(200).json(posts)
+    })
+    .catch(err =>{
+        console.log(err)
+        res.status(500).json({errorMessage: "error message"})
+    })
+})
+
+//get a specific post
+router.get(`/:id`, (req, res)=>{
+    Posts.findById(req.params.id)
+    .then(post => {
+        res.status(200).json(post)
+    })
+    .catch(err =>{
+        console.log(err)
+        res.status(500).json({errorMessage: "error message"})
+    })
+})
+
+//get all comments for a specific post
+router.get(`/:id/comments`, (req, res)=>{
+    Posts.findPostComments(req.params.id)
+    .then(posts => {
+        res.status(200).json(posts)
+    })
+    .catch(err =>{
+        console.log(err)
+        res.status(500).json({errorMessage: "error message"})
+    })
+})
+
+//delete a specific post
+router.delete(`/:id`, (req, res)=>{
+    Posts.remove(req.params.id)
+    .then(removedID => {
+        res.status(200).json(removedID)
+    })
+    .catch(err =>{
+        console.log(err)
+        res.status(500).json({errorMessage: "error message"})
+    })
+})
+
+//update a specific post
+router.put(`/:id`, (req, res)=>{
+    Posts.update(req.params.id, req.body)
+    .then(updatedCount => {
+        res.status(200).json(updatedCount)
+    })
+    .catch(err =>{
+        console.log(err)
+        res.status(500).json({errorMessage: "error message"})
+    })
+})
+
+module.exports = router;
